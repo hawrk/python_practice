bc7d2b0ae808d8cc465ce4c1348d78ec297efbb2
hawrk <hawrk2012@163.com>
2016-08-09 23:33:37 +0800

    add urllib example

---
+++ web/myurllib.py
@@ -0,0 +1,16 @@
+
+import urllib
+import urllib.request
+
+
+sock = urllib.request.urlopen('http://www.baidu.com')
+
+bytes = sock.read()
+mystr = bytes.decode('utf-8')
+
+print ('str =' + mystr)
+
+urlobj = urllib.request.urlretrieve('http://www.hao123.com.cn','hao123')
+
+
+sock.close()
